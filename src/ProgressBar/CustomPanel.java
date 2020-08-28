81bcd28368b045af92d413c1d1bbc475c3991077
YoonKraken <yoonda5898@gmail.com>
2020-08-29 01:08:47 +0900

    Complete practice
    reverse ProgressBar

--- src/ProgressBar/CustomPanel.java
+++ src/ProgressBar/CustomPanel.java
@@ -3,9 +3,11 @@ package ProgressBar;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Arc2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 public class CustomPanel extends JPanel {
-    int progress = 0;
+    int progress = 100;
     public void UpdateProgress(int progress){
         this.progress=progress;
     }
@@ -13,19 +15,38 @@ public class CustomPanel extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2D = (Graphics2D)g;
+        // 라인 부드럽게
+        g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         // 보이게 하기?
         g2D.translate(this.getWidth()/2, this.getHeight()/2);
         // 시계반대방향으로 움직이기때문에 rotate(회전)를 설정해준다.
         // x축방향에서 시작해서 시계방향으로 270도를 돌리는 것이다.
         g2D.rotate(Math.toRadians(270));
         Arc2D.Float arc = new Arc2D.Float(Arc2D.PIE);
+        // 가리는 원
+        Ellipse2D circle = new Ellipse2D.Float(0,0,110,110);
         arc.setFrameFromCenter(new Point(0,0),new Point(120,120));
+        circle.setFrameFromCenter(new Point(0,0),new Point(110,110));
         // 시작 각도
         arc.setAngleStart(1);
         // 범위 ; -:반대로
-        arc.setAngleExtent(-progress*(360/100));
+        arc.setAngleExtent(progress*3.6);// 360/100
+
+        // 그림
         g2D.setColor(Color.red);
         g2D.draw(arc);
         g2D.fill(arc);
+        g2D.setColor(Color.white);
+        g2D.draw(circle);
+        g2D.fill(circle);
+        // 글자
+        g2D.setColor(Color.DARK_GRAY);
+        g2D.rotate(Math.toRadians(90));
+        g.setFont(new Font("Verdana",Font.PLAIN,50));
+        FontMetrics fm = g2D.getFontMetrics();
+        Rectangle2D r = fm.getStringBounds(progress+"%",g);
+        int x = (0-(int)r.getWidth())/2; // 글자 폭의 반
+        int y = (0-(int)r.getHeight())/2+fm.getAscent(); // 글자 높이의 반 + y축값
+        g2D.drawString(progress+"%",x,y);
     }
 }
--- src/ProgressBar/ProgressBarDemo.java
+++ src/ProgressBar/ProgressBarDemo.java
@@ -5,41 +5,43 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ProgressBarDemo extends JFrame{
+public class ProgressBarDemo extends JFrame implements ActionListener {
     private JButton button1;
     Container contentPanel = this.getContentPane();
-    private JPanel jpanel = new CustomPanel();
+    private CustomPanel jpanel = new CustomPanel();
     private JPanel fir_panel = new JPanel();
 
     public ProgressBarDemo(){
         super("ProgressBar");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600,500);
         button1 = new JButton("Start");
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("click!");
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        for(int num=1; num<100; num++){
-                            //jpanel.UpdateProgress(num);
-                            jpanel.repaint();
-                            try {
-                                Thread.sleep(50);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }).start();
-            }
-        });
+        button1.addActionListener(this::actionPerformed);
 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jpanel.add(button1);
         add(jpanel);
         setVisible(true);
     }
 
+    public void actionPerformed(ActionEvent evt){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int num=100; num>=0; num--){
+                    jpanel.UpdateProgress(num);
+                    jpanel.repaint();
+                    try {
+                        Thread.sleep(50);
+                        if(num==0){
+                            jpanel.UpdateProgress(100);
+                            jpanel.repaint();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+    }
 }
