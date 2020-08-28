6510e12233dc4b8af92375dadef70d2b9bc8d6cd
YoonKraken <yoonda5898@gmail.com>
2020-08-29 01:08:47 +0900

    Complete practice
    reverse ProgressBar

--- src/ProgressBar/ProgressBar.java
+++ src/ProgressBar/ProgressBar.java
@@ -2,7 +2,7 @@ package ProgressBar;
 
 public class ProgressBar {
     public static void main(String[] args) {
-        ProgressBarDemo1 obj_frame = new ProgressBarDemo1();
+        ProgressBarDemo obj_frame = new ProgressBarDemo();
         obj_frame.setVisible(true);
     }
 }
