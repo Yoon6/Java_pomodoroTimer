a6c4fe01fdb954f9440bb278852e522d507ec3c8
YoonKraken <yoonda5898@gmail.com>
2020-09-07 23:33:47 +0900

    setting Layout 설정중
    using GridBagLayout

--- src/settingFrame.java
+++ src/settingFrame.java
@@ -9,23 +9,27 @@ public class settingFrame extends defaultFrame implements ActionListener{
     Image tmp_close = img_close.getImage().getScaledInstance(24,24,Image.SCALE_SMOOTH);
 
     settingPanel sp = new settingPanel();
+    JPanel panel = new JPanel();
 
     settingFrame(){
         super("Settings");
         setSize(300,200);
-        setLayout(new BorderLayout());
         setBackground(new Color(0,0,0,122));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
+        setLayout(new FlowLayout(FlowLayout.RIGHT));
         btn_close = new JButton(new ImageIcon(tmp_close));
         createButton(btn_close);
         btn_close.addActionListener(this);
 
+
         sp.setBackground(new Color(0,0,0,0));
+        panel.setBackground(new Color(0,0,0,0));
 
-        add(btn_close, BorderLayout.NORTH);
+
+
+        panel.add(btn_close);
+        add(panel);
         add(sp);
-
         setVisible(true);
     }
 
--- src/settingPanel.java
+++ src/settingPanel.java
@@ -1,20 +1,61 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ItemEvent;
 
 public class settingPanel extends JPanel {
     JComboBox<Integer> comboBox;
     static int ttime = 19;
-
+    JLabel label_combo;
+    JCheckBox auto;
+    JCheckBox routine;
+    JCheckBox sound;
 
     settingPanel(){
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill=GridBagConstraints.BOTH;
+        gbc.gridx=0;
+        gbc.gridy=0;
+
+        label_combo = new JLabel("Time Setting : ");
+        label_combo.setFont(new Font("Arial",Font.PLAIN,20));
+        label_combo.setForeground(Color.WHITE);
+        add(label_combo, gbc);
         comboBox = new JComboBox<Integer>();
         for(int i=1; i<=60; i++){
             comboBox.addItem(Integer.valueOf(i));
         }
         comboBox.addItemListener(this::itemStateChanged);
         comboBox.setSelectedIndex(ttime);
+        comboBox.setEditable(true);
+        gbc.gridx=1;
+        gbc.gridy=0;
+        add(comboBox, gbc);
 
-        add(comboBox);
+        auto = new JCheckBox("Automatically restart");
+        auto.setBackground(new Color(0,0,0,0));
+        auto.setFont(new Font("Arial",Font.PLAIN,20));
+        auto.setForeground(Color.WHITE);
+        gbc.gridx=0;
+        gbc.gridy=1;
+        add(auto, gbc);
+
+        routine = new JCheckBox("Use routine");
+        routine.setBackground(new Color(0,0,0,0));
+        routine.setFont(new Font("Arial",Font.PLAIN,20));
+        routine.setForeground(Color.WHITE);
+        gbc.gridx=0;
+        gbc.gridy=2;
+        add(routine, gbc);
+
+        sound = new JCheckBox("Sound on");
+        sound.setBackground(new Color(0,0,0,0));
+        sound.setFont(new Font("Arial",Font.PLAIN,20));
+        sound.setForeground(Color.WHITE);
+        gbc.gridx=0;
+        gbc.gridy=3;
+        add(sound, gbc);
+
     }
 
     public void itemStateChanged(ItemEvent ie) {
